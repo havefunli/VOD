eb31ab13bb9102a3ca58647491d007b22d9bbced
Jack <1641869338@qq.com>
2024-12-04 19:38:43 +0800

    测试SQL

--- tests/test-sqltable.cpp
+++ tests/test-sqltable.cpp
@@ -1,9 +1,69 @@
 #include "../include/SQLTable.h"
+#include <iostream>
+/*'知识' - 1,
+  '娱乐' - 2,
+  '生活' - 3,
+  '情感' - 4,
+  '才艺' - 5,
+  '剧情' - 6,
+  '社会' - 7,
+  '游戏' - 8
+*/
 
+SqlTable st;
+
+
+/*测试插入数据*/
+void test_1()
+{
+    Json::Value root;
+    root["name"] = "加菲猫的幸福生活";
+    root["description"] = "一只猫和主人和一只小狗的幸福生活";
+    root["type"] = 2;
+    root["video_path"] = "./";
+    root["cover_path"] = "./";
+    st.Insert(root);
+}
+
+/*测试更新数据*/
+void test_2()
+{
+    Json::Value root;
+    root["name"] = "海绵宝宝";
+    root["description"] = "海底世界一群动物的生活";
+    root["type"] = 2;
+    st.Update(1, root);
+}
+
+/*查询数据*/
+void test_3()
+{
+    Json::Value root;
+    st.SelectAll(root);
+    std::cout << root.toStyledString() << std::endl;
+}
+
+void test_4()
+{
+    Json::Value root;
+    st.SelectOne(1, root);
+    std::cout << root.toStyledString() << std::endl;
+}
+
+void test_5()
+{
+    Json::Value root;
+    std::pair<std::string, std::string> pair("name", "海绵宝宝");
+    st.SelectLike(pair, root);
+    std::cout << root.toStyledString() << std::endl;
+}
 
 int main()
 {
-    MYSQL* conn = SqlTable::SQLConnect();
-    SqlTable::SQLDestroy(conn);
+    // test_1();
+    // test_2();
+    // test_3();
+    test_4();
+    test_5();
     return 0;
 }

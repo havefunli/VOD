2edfc0be45f1404d5b69f8798ec89e598076a55f
Jack <1641869338@qq.com>
2024-12-04 19:38:24 +0800

    新增了增删查改功能

--- include/SQLTable.h
+++ include/SQLTable.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "../src/Util.hpp"
+#include <utility>
 #include <mysql/mysql.h>
+#include <mutex>
 
 // 全局变量声明
 extern const char *DEFAULT_HOST;
@@ -9,9 +11,14 @@ extern const char *DEFAULT_PASSWD;
 extern const char *DEFAULT_DB;
 extern const uint16_t DEFAULT_PORT;
 extern const char *DEFAULT_CHAR;
+extern const int DEFAULT_SIZE;
+
+using StringPair = std::pair<std::string, std::string>;
 
 class SqlTable
 {
+private:
+    bool FetchResults(const std::string&, Json::Value&);
 public:
     /*连接mysql数据库特定的数据表*/
     static MYSQL *SQLConnect(const char* database = DEFAULT_DB);
@@ -19,4 +26,25 @@ public:
     static void SQLDestroy(MYSQL* conn);
     /*数据库执行*/
     static bool SQLQuery(MYSQL* conn, const std::string& cmd);
+    
+    /*初始化函数*/
+    SqlTable();
+    /*析构函数*/
+    ~SqlTable();
+
+    /*添加数据*/
+    bool Insert(const Json::Value&);
+    /*删除数据*/
+    bool Delete(int);
+    /*修改数据*/
+    bool Update(int, const Json::Value&);
+    /*查询数据*/
+    bool SelectAll(Json::Value&);
+    bool SelectOne(int, Json::Value&);
+    bool SelectLike(const StringPair&, Json::Value&);
+
+
+private:
+    MYSQL* _conn;
+    std::mutex _mtx;
 };
--- src/SQLTable.cpp
+++ src/SQLTable.cpp
@@ -7,6 +7,7 @@ const char *DEFAULT_PASSWD = "20040823@Lq";
 const char *DEFAULT_DB = "VOD";
 const uint16_t DEFAULT_PORT = 0;
 const char *DEFAULT_CHAR = "utf8";
+const int DEFAULT_SIZE = 4096;
 
 MYSQL *SqlTable::SQLConnect(const char* database)
 {
@@ -52,5 +53,109 @@ bool SqlTable::SQLQuery(MYSQL* conn, const std::string& cmd)
     }
     return true;
 }
+
+SqlTable::SqlTable()
+    : _conn(SQLConnect())
+{}
+
+SqlTable::~SqlTable()
+{
+    SQLDestroy(_conn);
+}
+
+bool SqlTable::Insert(const Json::Value& root)
+{
+    char tmp[DEFAULT_SIZE] = { '\0' };
+    /*id name description type video_path cover_path*/
+    snprintf(tmp, DEFAULT_SIZE, "insert into tb_video(name, description, type, video_path, cover_path) \
+                                        values('%s', '%s', %d, '%s', '%s');", 
+                                        root["name"].asCString(), 
+                                        root["description"].asCString(),
+                                        root["type"].asInt(),
+                                        root["video_path"].asCString(),
+                                        root["cover_path"].asCString());
+    return SQLQuery(_conn, tmp);
+}
+ 
+bool SqlTable::Delete(int video_id)
+{
+    char tmp[DEFAULT_SIZE] = { '\0' };
+    snprintf(tmp, DEFAULT_SIZE, "delete from tb_video where id = %d", video_id);
+    return SQLQuery(_conn, tmp);
+}
+
+bool SqlTable::Update(int video_id, const Json::Value& root)
+{
+    char tmp[DEFAULT_SIZE] = { '\0' };
+    /*id name description type video_path cover_path*/
+    snprintf(tmp, DEFAULT_SIZE, "update tb_video set name = '%s', description = '%s', type = %d where id = %d", 
+                                                root["name"].asCString(), 
+                                                root["description"].asCString(), 
+                                                root["type"].asInt(),
+                                                video_id);
+    return SQLQuery(_conn, tmp);
+}
+
+bool SqlTable::FetchResults(const std::string& query, Json::Value& root)
+{
+    MYSQL_RES* res;
+    /*保证线程安全*/
+    {
+        std::unique_lock<std::mutex> lck(_mtx);
+        bool ret = SQLQuery(_conn, query.c_str());
+        
+        if (!ret) { return false; }
+        res = mysql_store_result(_conn);
+    }
+    if (res == nullptr)
+    {
+        spdlog::error("mysql_store_result error, info = {}", mysql_error(_conn));
+        return false;
+    }
+
+    int num_rows = mysql_num_rows(res);
+    for (int i = 0; i < num_rows; i++)
+    {
+        /*id name description type video_path cover_path*/
+        MYSQL_ROW row = mysql_fetch_row(res);
+        Json::Value data;
+        data["id"] = row[0];
+        data["name"] = row[1];
+        data["description"] = row[2];
+        data["type"] = row[3];
+        data["video_path"] = row[4];
+        data["cover_path"] = row[5];
+        root.append(data);
+    }
+    
+    mysql_free_result(res);
+    return true;
+}
+
+bool SqlTable::SelectAll(Json::Value& root)
+{
+    return FetchResults("select * from tb_video;", root);
+}
+
+bool SqlTable::SelectOne(int video_id, Json::Value& root)
+{
+    Json::Value result;
+    char tmp[DEFAULT_SIZE];
+    snprintf(tmp, DEFAULT_SIZE, "select * from tb_video where id = %d", video_id);
+    if (!FetchResults(tmp, result)) { return false; }
+    root = result[0];
+    return true;
+}
+
+bool SqlTable::SelectLike(const StringPair& pair, Json::Value& root)
+{
+    char tmp[DEFAULT_SIZE];
+    snprintf(tmp, DEFAULT_SIZE, "select * from tb_video where %s = '%s'", 
+                                pair.first.c_str(), 
+                                pair.second.c_str());
+    if (!FetchResults(tmp, root)) { return false; }
+    return true;
+}
+
 
 
